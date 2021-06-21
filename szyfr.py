af27203311c891c174835ac80f497c1765734c5a
Hvenom-py <86004555+Hvenom-py@users.noreply.github.com>
2021-06-21 14:56:57 +0200

    Update szyfr.py

--- szyfr.py
+++ szyfr.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+massage = input("Enter massage to code\n")
+
+
+def code(massage):
+    nowe=""
+    for x in massage:
+        x = chr(ord(x)+3)
+        nowe+=x
+        
+    return nowe 
+
+
+print(code(massage))
+
+
