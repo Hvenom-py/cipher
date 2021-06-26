d515d5c8884594397fcfcf3804ab7de46fce02d2
Hvenom-py <86004555+Hvenom-py@users.noreply.github.com>
2021-06-26 13:04:14 +0200

    Update szyfr.py

--- szyfr.py
+++ szyfr.py
@@ -1,21 +1,9 @@
-import random
-
-massage = input("Enter massage to code\n")
-
-
-def code(massage):
-    nowe=""
-    for x in massage:
-        rnd=random.randint(0,366)
-        x = chr(ord(x)+rnd)
-        nowe+=x
-        
-    return nowe 
-
-
-print(code(massage))
-
-
+zmienna = ord('x')
+print(zmienna)
+print(chr(zmienna))
+zmienna-=55
+print(zmienna)
+print(chr(zmienna))
 
 
 
