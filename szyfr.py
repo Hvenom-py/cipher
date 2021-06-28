c5d255bccb2064e9c6caab8343853b06b71367fc
Hvenom-py <86004555+Hvenom-py@users.noreply.github.com>
2021-06-28 21:39:07 +0200

    Update szyfr.py

--- szyfr.py
+++ szyfr.py
@@ -1,9 +1,18 @@
-zmienna = ord('x')
-print(zmienna)
-print(chr(zmienna))
-zmienna-=55
-print(zmienna)
-print(chr(zmienna))
+import random
+
+slowo = "szyfr"
+
+def szyfrowanie(slowo):
+    nowe = ""
+    for x in slowo:
+        rnd = (random.randint(1,366))%25
+        if ord(x)+rnd > 122:
+            x=ord(x)+rnd-25
+            nowe+=chr(x)   
+        else:
+            x=ord(x)+rnd
+            nowe+=chr(x)   
+    return nowe
 
 
-
+print(szyfrowanie(slowo))
